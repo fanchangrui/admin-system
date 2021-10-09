f74ad492998a0ad8f411b3382a2000a61dbf1270
fanchangrui <1293707902@qq.com>
2021-10-09 14:39:53 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -8,14 +8,22 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
-
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const app = createApp(App)
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  NProgress.start()
   return config
 })
+axios.interceptors.response.use(
+  (config) => {
+    NProgress.done()
+    return config
+  }
+)
 app.config.globalProperties.$axios = axios
 app.use(router)
 app.use(ElementPlus)
