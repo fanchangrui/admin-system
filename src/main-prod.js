d23207b24431132591edccd6c403f541262d3107
fanchangrui <1293707902@qq.com>
2021-10-09 15:07:18 +0800

    发布

---
+++ src/main-prod.js
@@ -0,0 +1,32 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/css/global.css'
+import ElementPlus from 'element-plus';
+import 'element-plus/theme-chalk/index.css';
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+const app = createApp(App)
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  NProgress.start()
+  return config
+})
+axios.interceptors.response.use(
+  (config) => {
+    NProgress.done()
+    return config
+  }
+)
+app.config.globalProperties.$axios = axios
+app.use(router)
+app.use(ElementPlus)
+app.component('QuillEditor', QuillEditor)
+
+app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,23 @@
-module.exports ={
-    lintOnSave: false
+const { tap } = require("lodash")
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+      }
+    }
+  },
+  // 发布模式
+  chainWebpack: config => {
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+
+    })
+
+    // 开发模式
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+
+    })
+  }
 }
-/*
-vue配置*/
